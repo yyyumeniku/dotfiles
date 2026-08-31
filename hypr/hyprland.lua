488f46a8a3d1aa32ca7666e9c3115e26f10170e8
Gabriel Hernandez <146843738+yyyumeniku@users.noreply.github.com>
2026-08-31 00:04:12 -0400

    Add NVIDIA driver environment variables to config

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -62,6 +62,9 @@ end)
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("GBM_BACKEND", "nvidia-drm")
 
 
 -----------------------
